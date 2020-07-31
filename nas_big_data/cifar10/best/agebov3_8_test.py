3b599f590c80efa29a8f6d10019084234314bd33
Deathn0t <romainegele@gmail.com>
2020-07-31 16:19:45 +0200

    added test train for agebov3 in cifar10

---
+++ nas_big_data/cifar10/best/agebov3_8_test.py
@@ -0,0 +1,57 @@
+"""
+KMP_BLOCK_TIME=0
+
+
+horovodrun -np 4 python -m deephyper.benchmark.nas.covertype.train
+"""
+
+from nas_big_data.cifar10.problem_agebov3 import Problem
+from nas_big_data.cifar10.load_data import load_data
+from deephyper.search.nas.model.run.horovod import run
+
+Problem.load_data(load_data, use_test=True)
+config = Problem.space
+
+config["hyperparameters"]["num_epochs"] = 100
+config["hyperparameters"]["learning_rate"] = 0.001061618285394656
+config["hyperparameters"]["batch_size"] = 128  # 1 rank
+config["hyperparameters"]["callbacks"].pop("TimeStopping")
+config["hyperparameters"]["verbose"] = 1
+
+
+config["arch_seq"] = [
+    8,
+    3,
+    0,
+    1,
+    7,
+    9,
+    1,
+    1,
+    7,
+    3,
+    1,
+    3,
+    0,
+    1,
+    4,
+    4,
+    4,
+    9,
+    0,
+    0,
+    0,
+    5,
+    1,
+    2,
+    2,
+    3,
+    3,
+    0,
+    4,
+    6,
+    3,
+    3,
+]
+
+run(config)
