e3bac4d61526f32229bca365a1127c97acd0b5fb
Deathn0t <romainegele@gmail.com>
2020-07-31 15:45:34 +0200

    updated best agebov3 in cifar10

--- nas_big_data/cifar10/best/agebov3_8.py
+++ nas_big_data/cifar10/best/agebov3_8.py
@@ -13,6 +13,7 @@ config = Problem.space
 config["hyperparameters"]["num_epochs"] = 100
 config["hyperparameters"]["learning_rate"] = 0.001061618285394656
 config["hyperparameters"]["batch_size"] = 128  # 1 rank
+config["hyperparameters"]["callbacks"].pop("TimeStopping")
 
 
 config["arch_seq"] = [
