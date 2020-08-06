78e500f62ba6fde0923e11b8b36bfc2796d08fd4
Deathn0t <romainegele@gmail.com>
2020-08-06 14:44:08 +0200

    add airlines problems

---
+++ nas_big_data/airlines/problem_agebov1.py
@@ -0,0 +1,32 @@
+from deephyper.problem import NaProblem
+from nas_big_data.airlines.dense_skipco import create_search_space
+from nas_big_data.airlines.load_data import load_data
+
+
+Problem = NaProblem(seed=2019)
+
+Problem.load_data(load_data)
+
+Problem.search_space(create_search_space, num_layers=10)
+
+Problem.hyperparameters(
+    batch_size=256,
+    learning_rate=(0.001, 0.1, "log-uniform"),
+    optimizer="adam",
+    num_epochs=20,
+    verbose=0,
+    callbacks=dict(CSVExtendedLogger=dict()),
+)
+
+Problem.loss("categorical_crossentropy")
+
+Problem.metrics(["acc"])
+
+Problem.objective("val_acc")
+
+
+# Just to print your problem, to test its definition and imports in the current python environment.
+if __name__ == "__main__":
+    print(Problem)
+
+    # model = Problem.get_keras_model([4 for _ in range(20)])
---
+++ nas_big_data/airlines/problem_agebov3.py
@@ -0,0 +1,43 @@
+from deephyper.problem import NaProblem
+from nas_big_data.airlines.dense_skipco import create_search_space
+from nas_big_data.airlines.load_data import load_data
+
+Problem = NaProblem(seed=2019)
+
+Problem.load_data(load_data)
+
+Problem.search_space(create_search_space, num_layers=10)
+
+
+Problem.hyperparameters(
+    batch_size=[32, 64, 128, 256, 512, 1024],
+    learning_rate=(0.001, 0.1, "log-uniform"),
+    optimizer="adam",
+    num_epochs=20,  # maximal bound
+    verbose=0,
+    callbacks=dict(
+        CSVExtendedLogger=dict(),
+        ModelCheckpoint=dict(
+            monitor="val_acc",
+            mode="max",
+            save_best_only=True,
+            verbose=0,
+            filepath="model.h5",
+            save_weights_only=True,
+        ),
+    ),
+    ranks_per_node=[1, 2, 4, 8],
+)
+
+Problem.loss("categorical_crossentropy")
+
+Problem.metrics(["acc"])
+
+Problem.objective("val_acc")
+
+
+# Just to print your problem, to test its definition and imports in the current python environment.
+if __name__ == "__main__":
+    print(Problem)
+
+    # model = Problem.get_keras_model([4 for _ in range(20)])
