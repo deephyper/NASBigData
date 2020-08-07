fa392162a118595e50663519437c4efc211ce776
Deathn0t <romainegele@gmail.com>
2020-08-07 10:23:06 +0200

    added autogluon evaluation for covertype

---
+++ nas_big_data/covertype/autogluon/evaluate.py
@@ -0,0 +1,31 @@
+import os
+import pathlib
+
+from autogluon import TabularPrediction as task
+from nas_big_data.covertype.load_data import load_data
+from nas_big_data.data_utils import convert_to_dataframe
+
+here = os.path.dirname(os.path.abspath(__file__))
+output_dir = os.path.join(here, "outputs")
+
+# Create output directory
+pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+(X_train, y_train), (X_test, y_test) = load_data(use_test=True)
+
+df_train = convert_to_dataframe(X_train, y_train)
+
+predictor = task.fit(
+    train_data=task.Dataset(df=df_train), label="label", output_directory=output_dir
+)
+
+df_test = convert_to_dataframe(X_test, y_test)
+
+y_pred = predictor.predict(task.Dataset(df=df_test))
+
+y_test = df_test.label
+
+perf = predictor.evaluate_predictions(
+    y_true=y_test, y_pred=y_pred, auxiliary_metrics=True
+)
+print(perf)
