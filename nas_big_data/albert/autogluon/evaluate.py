f6a8fc55d05d4606154905ed8c335fff757f37a1
Deathn0t <romainegele@gmail.com>
2020-08-10 10:21:02 +0200

    updated autogluon evaluation

--- nas_big_data/albert/autogluon/evaluate.py
+++ nas_big_data/albert/autogluon/evaluate.py
@@ -14,45 +14,56 @@ parser = argparse.ArgumentParser(description="Process some integers.")
 parser.add_argument(
     "--walltime", type=int, default=30 * 60, help="Walltime to fit AutoGluon"
 )
+parser.add_argument(
+    "--evaluate", const=True, nargs="?", default=False, help="Evaluate model."
+)
 
 args = parser.parse_args()
 
-if args.walltime <= 120:
-    excluded_model_types = ["KNN"]
+if not args.evaluate:
+    if args.walltime <= 120:
+        excluded_model_types = ["KNN"]
+    else:
+        excluded_model_types = []
+
+    # Create output directory
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    (X_train, y_train), (X_valid, y_valid) = load_data(use_test=False)
+
+    df_train = convert_to_dataframe(X_train, y_train)
+    df_valid = convert_to_dataframe(X_valid, y_valid)
+
+    predictor = task.fit(
+        train_data=task.Dataset(df=df_train),
+        tuning_data=task.Dataset(df=df_valid),
+        label="label",
+        output_directory=output_dir,
+        time_limits=args.walltime,
+        hyperparameter_tune=True,
+        auto_stack=True,
+        excluded_model_types=excluded_model_types,
+    )
 else:
-    excluded_model_types = []
+    _, (X_test, y_test) = load_data(use_test=True)
 
-# Create output directory
-pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    print("Convert arrays to DataFrame...")
+    df_test = convert_to_dataframe(X_test, y_test)
 
-(X_train, y_train), (X_valid, y_valid) = load_data(use_test=False)
-_, (X_test, y_test) = load_data(use_test=True)
+    print("Loading models...")
+    predictor = task.load(output_dir, verbosity=4)
 
-df_train = convert_to_dataframe(X_train, y_train)
-df_valid = convert_to_dataframe(X_valid, y_valid)
+    print("Predicting...")
+    y_pred = predictor.predict(task.Dataset(df=df_test))
 
-predictor = task.fit(
-    train_data=task.Dataset(df=df_train),
-    tuning_data=task.Dataset(df=df_valid),
-    label="label",
-    output_directory=output_dir,
-    time_limits=args.walltime,
-    hyperparameter_tune=True,
-    auto_stack=True,
-    excluded_model_types=excluded_model_types,
-)
+    y_test = df_test.label
 
-df_test = convert_to_dataframe(X_test, y_test)
+    print("Evaluation predictions...")
+    results = predictor.evaluate_predictions(
+        y_true=y_test, y_pred=y_pred, auxiliary_metrics=True
+    )
+    print(results)
 
-y_pred = predictor.predict(task.Dataset(df=df_test))
-
-y_test = df_test.label
-
-results = predictor.evaluate_predictions(
-    y_true=y_test, y_pred=y_pred, auxiliary_metrics=True
-)
-print(results)
-
-test_scores_path = os.path.join(here, "test_scores.json")
-with open(test_scores_path, "w") as fp:
-    json.dump(results, fp)
+    test_scores_path = os.path.join(here, "test_scores.json")
+    with open(test_scores_path, "w") as fp:
+        json.dump(results, fp)
