1291d3aa248179ac5fdce992a7ae217a05b39b7a
Deathn0t <romainegele@gmail.com>
2020-08-12 13:19:08 +0200

    update

--- nas_big_data/covertype/autogluon/evaluate_theta.py
+++ nas_big_data/covertype/autogluon/evaluate_theta.py
@@ -76,6 +76,21 @@ if not args.evaluate:
     df_train = convert_to_dataframe(X_train, y_train)
     df_valid = convert_to_dataframe(X_valid, y_valid)
 
+    # hyperparameters
+    nunits = list(range(16, 97, 16))
+    nn_options = {  # specifies non-default hyperparameter values for neural network models
+        "num_epochs": 100,  # number of training epochs (controls training time of NN models)
+        "learning_rate": ag.space.Real(
+            0.001, 0.1, default=0.01, log=True
+        ),  # learning rate used in training (real-valued hyperparameter searched on log-scale)
+        "activation": ag.space.Categorical(
+            None, swish, "relu", "tanh", "sigmoid"
+        ),  # activation function used in NN (categorical hyperparameter, default = first entry)
+        "layers": ag.space.Categorical(*(nunits for _ in range(10))),
+        # Each choice for categorical hyperparameter 'layers' corresponds to list of sizes for each NN layer to use
+        "dropout_prob": 0.0,
+    }
+
     predictor = task.fit(
         train_data=task.Dataset(df=df_train),
         tuning_data=task.Dataset(df=df_valid),
