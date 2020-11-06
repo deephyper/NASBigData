99ea3d1c3f35ddc38c11f9014d50634008545fbf
Deathn0t <romainegele@gmail.com>
2020-11-06 15:28:28 +0100

    updated create_search_space function for covertype

--- nas_big_data/covertype/dense_skipco.py
+++ nas_big_data/covertype/dense_skipco.py
@@ -1,7 +1,8 @@
 from deepspace.tabular import DenseSkipCoFactory
 
-def_kwargs = dict(input_shape=(54,), output_shape=(7,), num_layers=10, dropout=0.0)
-create_search_space = lambda *x: DenseSkipCoFactory(*x).create_space()
+
+def create_search_space(input_shape=(54,), output_shape=(7,), num_layers=10, dropout=0.0):
+    return DenseSkipCoFactory(**locals()).create_space()
 
 
 def test_create_search_space():
