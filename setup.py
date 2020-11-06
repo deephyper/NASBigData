06159b0cac9ba0bfec257a6e0af536762bc7fa43
Deathn0t <romainegele@gmail.com>
2020-11-06 20:46:15 +0100

    updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nas_big_data", packages=find_packages(), install_requires=["deepspace==0.0.1"]
+    name="nas_big_data", packages=find_packages(), install_requires=["deepspace==0.0.2"]
 )
