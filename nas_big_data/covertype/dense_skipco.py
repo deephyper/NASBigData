0c82c2d08ee7f03be8b5b0cca8a73148d5b0c89f
Deathn0t <romainegele@gmail.com>
2020-11-06 15:08:31 +0100

    switching search space to new style factory

--- nas_big_data/covertype/dense_skipco.py
+++ nas_big_data/covertype/dense_skipco.py
@@ -1,63 +1,7 @@
-import collections
+from deepspace.tabular import DenseSkipCoFactory
 
-import tensorflow as tf
-
-from deephyper.nas.space import AutoKSearchSpace
-from deephyper.nas.space.node import ConstantNode, VariableNode, MimeNode
-from deephyper.nas.space.op.basic import Tensor, Zero
-from deephyper.nas.space.op.connect import Connect
-from deephyper.nas.space.op.merge import AddByProjecting
-from deephyper.nas.space.op.op1d import Dense, Identity, Dropout
-
-
-def add_dense_to_(node):
-    node.add_op(Identity())  # we do not want to create a layer in this case
-
-    activations = [None, tf.nn.swish, tf.nn.relu, tf.nn.tanh, tf.nn.sigmoid]
-    for units in range(16, 97, 16):
-        for activation in activations:
-            node.add_op(Dense(units=units, activation=activation))
-
-
-def create_search_space(
-    input_shape=(54,), output_shape=(7,), num_layers=10, dropout=0.0, *args, **kwargs
-):
-
-    regression = False
-    ss = AutoKSearchSpace(input_shape, output_shape, regression=regression)
-    source = prev_input = ss.input_nodes[0]
-
-    # look over skip connections within a range of the 3 previous nodes
-    anchor_points = collections.deque([source], maxlen=3)
-
-    for _ in range(num_layers):
-        vnode = VariableNode()
-        add_dense_to_(vnode)
-
-        ss.connect(prev_input, vnode)
-
-        # * Cell output
-        cell_output = vnode
-
-        cmerge = ConstantNode()
-        cmerge.set_op(AddByProjecting(ss, [cell_output], activation="relu"))
-
-        for anchor in anchor_points:
-            skipco = VariableNode()
-            skipco.add_op(Zero())
-            skipco.add_op(Connect(ss, anchor))
-            ss.connect(skipco, cmerge)
-
-        prev_input = cmerge
-
-        # ! for next iter
-        anchor_points.append(prev_input)
-
-    if dropout >= 0.0:
-        dropout_node = ConstantNode(op=Dropout(rate=dropout))
-        ss.connect(prev_input, dropout_node)
-
-    return ss
+def_kwargs = dict(input_shape=(54,), output_shape=(7,), num_layers=10, dropout=0.0)
+create_search_space = lambda *x: DenseSkipCoFactory(*x).create_space()
 
 
 def test_create_search_space():
--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nas_big_data',
-    packages=find_packages(),
-    install_requires=[]
+    name="nas_big_data", packages=find_packages(), install_requires=["deepspace==0.0.1"]
 )
