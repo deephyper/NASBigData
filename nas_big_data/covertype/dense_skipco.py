718ca19b42d240a3f0e48eaf4652c86b109e1808
Deathn0t <romainegele@gmail.com>
2020-11-07 20:15:24 +0100

    updated covertype dense search space to be compatible with SpaceFactory __call__, build interface

--- nas_big_data/covertype/dense_skipco.py
+++ nas_big_data/covertype/dense_skipco.py
@@ -4,8 +4,9 @@ from deepspace.tabular import DenseSkipCoFactory
 def create_search_space(
     input_shape=(54,), output_shape=(7,), num_layers=10, dropout=0.0, **kwargs
 ):
-    kwargs.update({k: v for k, v in locals().items() if k != "kwargs"})
-    return DenseSkipCoFactory(**kwargs).create_space()
+    return DenseSkipCoFactory()(
+        input_shape, output_shape, num_layers=num_layers, dropout=dropout, **kwargs
+    )
 
 
 def test_create_search_space():
