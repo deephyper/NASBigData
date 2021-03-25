32d208fb212d1804cc2cc50510f0cab3032205e6
Deathn0t <romainegele@gmail.com>
2021-03-25 14:55:55 +0100

    adding problem_test to combo

---
+++ nas_big_data/combo/problem_test.py
@@ -0,0 +1,34 @@
+from deephyper.problem import NaProblem
+from nas_big_data.combo.search_space import create_search_space
+from nas_big_data.combo.load_data import load_data
+
+Problem = NaProblem(seed=2019)
+
+Problem.load_data(load_data)
+
+Problem.search_space(create_search_space, num_layers=5)
+
+Problem.hyperparameters(
+    batch_size=32,
+    learning_rate=0.001,
+    optimizer="adam",
+    num_epochs=4,
+    verbose=1,
+    callbacks=dict(
+        ReduceLROnPlateau=dict(monitor="val_r2", mode="max", verbose=0, patience=5),
+        EarlyStopping=dict(
+            monitor="val_r2", min_delta=0, mode="max", verbose=0, patience=10
+        ),
+        )
+)
+
+Problem.loss("mse")
+
+Problem.metrics(["r2"])
+
+Problem.objective("val_r2")
+
+
+# Just to print your problem, to test its definition and imports in the current python environment.
+if __name__ == "__main__":
+    print(Problem)
