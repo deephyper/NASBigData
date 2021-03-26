f08f84e3be0fcb5443a597a3202e6e4df747883f
Deathn0t <romainegele@gmail.com>
2021-03-26 14:11:50 +0100

    added test for ray horovod

---
+++ tests/test_ray_horovod.py
@@ -0,0 +1,55 @@
+import ray
+
+import horovod.tensorflow.keras as hvd
+# import horovod.tensorflow as hvd
+from horovod.ray import RayExecutor
+
+# Start the Ray cluster or attach to an existing Ray cluster
+ray.init(address="auto")
+
+# Ray executor settings
+setting = RayExecutor.create_settings(timeout_s=100)
+num_hosts = 1  # number of machine to use
+num_slots = 8  # number of workers to use on each machine
+cpus_per_slot = 1  # number of cores to allocate to each worker
+gpus_per_slot = 1 # number of GPUs to allocate to each worker
+
+# Start num_hosts * num_slots actors on the cluster
+# https://horovod.readthedocs.io/en/stable/api.html#horovod-ray-api
+executor = RayExecutor(
+    setting,
+    num_hosts=num_hosts,
+    num_slots=num_slots,
+    cpus_per_slot=cpus_per_slot,
+    gpus_per_slot=gpus_per_slot,
+    use_gpu=True
+)
+
+# Launch the Ray actors on each machine
+# This will launch `num_slots` actors on each machine
+executor.start()
+
+
+#! Note that there is an implicit assumption on the cluster being
+#! homogenous in shape (i.e., all machines have the same number of
+#! slots available). This is simply an implementation detail and is
+#! not a fundamental limitation.
+
+# Using the stateless `run` method, a function can take in any args or kwargs
+def simple_fn():
+
+
+    hvd.init()
+
+    print("hvd rank", hvd.rank())
+    return 0
+
+
+# Execute the function on all workers at once
+result = executor.run(simple_fn)
+print(result)
+
+# Check that the rank of all workers is unique
+assert len(set(result)) == num_hosts * num_slots
+
+executor.shutdown()
