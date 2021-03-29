0b62b7281d984afa43d069d78fb5415529f99456
Deathn0t <romainegele@gmail.com>
2021-03-29 11:22:58 +0200

    added agebo test

---
+++ nas_big_data/combo/problem_agebo_test.py
@@ -0,0 +1,55 @@
+from deephyper.problem import NaProblem
+from nas_big_data.combo.search_space import create_search_space
+
+# from nas_big_data.combo.load_data import load_data
+from deephyper.benchmark.nas.linearReg.load_data import load_data
+
+Problem = NaProblem(seed=2019)
+
+Problem.load_data(load_data)
+
+Problem.search_space(create_search_space, num_layers=3)
+
+# losses = ["mae", "mse", "huber_loss", "log_cosh", "mean_absolute_percentage_error", "mean_squared_logarithmic_error"]
+# losses = ["mae", "mse", "huber_loss", "log_cosh", "mape", "msle"]
+# schedules: https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/schedules
+
+Problem.hyperparameters(
+    batch_size=Problem.add_hyperparameter((16, 2048, "log-uniform"), "batch_size"),
+    learning_rate=Problem.add_hyperparameter(
+        (1e-4, 0.01, "log-uniform"),
+        "learning_rate",
+    ),
+    optimizer=Problem.add_hyperparameter(
+        ["sgd", "rmsprop", "adagrad", "adam", "adadelta", "adamax", "nadam"], "optimizer"
+    ),
+    patience_ReduceLROnPlateau=Problem.add_hyperparameter(
+        (3, 30), "patience_ReduceLROnPlateau"
+    ),
+    patience_EarlyStopping=Problem.add_hyperparameter((3, 30), "patience_EarlyStopping"),
+    num_epochs=1,
+    verbose=0,
+    callbacks=dict(
+        ReduceLROnPlateau=dict(monitor="val_r2", mode="max", verbose=0, patience=5),
+        EarlyStopping=dict(
+            monitor="val_r2", min_delta=0, mode="max", verbose=0, patience=10
+        ),
+    ),
+)
+
+Problem.loss(
+    Problem.add_hyperparameter(
+        ["mae", "mse", "huber_loss", "log_cosh", "mape", "msle"], "loss"
+    )
+)
+
+Problem.metrics(["r2"])
+
+Problem.objective("val_r2")
+
+
+# Just to print your problem, to test its definition and imports in the current python environment.
+if __name__ == "__main__":
+    print(Problem)
+
+    rdm_config = Problem._hp_space._space.sample_configuration().get_dictionary()
