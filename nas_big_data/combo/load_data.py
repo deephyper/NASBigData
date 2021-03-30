956bd70b0f34d7e92d8caa1a2b14250a343eb18e
Deathn0t <romainegele@gmail.com>
2021-03-30 13:47:35 +0200

    caching data for combo

--- nas_big_data/combo/load_data.py
+++ nas_big_data/combo/load_data.py
@@ -1,10 +1,8 @@
 import os
 import gzip
 import numpy as np
-from numpy.lib.npyio import load
 
-from sklearn.model_selection import train_test_split
-from deephyper.benchmark.datasets.util import cache_load_data
+from nas_big_data.data_utils import cache_load_data
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -73,6 +71,25 @@ def load_data():
 def load_data_cache():
     return load_data()
 
+def test_load_data_cache():
+    from time import time
+    t1 = time()
+    load_data()
+    t2 = time()
+    dur = t2 - t1
+    print("Normal loading: ", dur) # -> ~ 35 sec
+
+    t1 = time()
+    load_data_cache()
+    t2 = time()
+    dur = t2 - t1
+    print("Cache call 1 loading: ", dur) # -> 45 sec
+
+    t1 = time()
+    load_data_cache()
+    t2 = time()
+    dur = t2 - t1
+    print("Cache call 2 loading: ", dur) # -> 2 sec
 
 if __name__ == "__main__":
     load_data()
--- nas_big_data/data_utils.py
+++ nas_big_data/data_utils.py
@@ -1,3 +1,7 @@
+import os
+import numpy as np
+
+
 def convert_to_dataframe(X, y):
     """Convert a X(inputs), y(outputs) dataset to a dataframe format.
 
@@ -17,3 +21,44 @@ def convert_to_dataframe(X, y):
     )
 
     return df
+
+
+def cache_load_data(cache_loc):
+    """Decorator of load_data function to dache numpy arrays return by the function. The load_data function should return a tuple of the form: ``(X_train, y_train), (X_valid, y_valid)``.
+
+    Args:
+        cache_loc (str): path where the data will be cached.
+    """
+
+    def _cache(data_loader):
+        def wrapper(*args, **kwargs):
+            if os.path.exists(cache_loc):
+                print("Reading data from cache")
+                with open(cache_loc, "rb") as fp:
+                    data = {k: arr for k, arr in np.load(fp, allow_pickle=True).item().items()}
+                return (
+                    (data["X_train"], data["y_train"]),
+                    (data["X_valid"], data["y_valid"]),
+                )
+
+            else:
+                (X_train, y_train), (X_valid, y_valid) = data_loader(*args, **kwargs)
+                if os.path.exists(os.path.dirname(cache_loc)):
+                    print("Data not cached; invoking user data loader")
+                    data = {
+                        "X_train": X_train,
+                        "y_train": y_train,
+                        "X_valid": X_valid,
+                        "y_valid": y_valid,
+                    }
+                    with open(cache_loc, "wb") as fp:
+                        np.save(fp, data)
+                else:
+                    print(
+                        "Data cannot be cached because the path does not exist. Returning data anyway."
+                    )
+                return (X_train, y_train), (X_valid, y_valid)
+
+        return wrapper
+
+    return _cache
