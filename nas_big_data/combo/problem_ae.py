e9fcaa4eac1c875cfeed499493281068f0fab342
Deathn0t <romainegele@gmail.com>
2021-04-01 09:15:51 +0200

    update

--- nas_big_data/combo/problem_ae.py
+++ nas_big_data/combo/problem_ae.py
@@ -1,10 +1,10 @@
 from deephyper.problem import NaProblem
-from nas_big_data.combo.search_space import create_search_space
-from nas_big_data.combo.load_data import load_data
+from nas_big_data.combo.search_space_shared import create_search_space
+from nas_big_data.combo.load_data import load_data_cache
 
 Problem = NaProblem(seed=2019)
 
-Problem.load_data(load_data)
+Problem.load_data(load_data_cache)
 
 Problem.search_space(create_search_space, num_layers=5)
 
@@ -17,7 +17,6 @@ Problem.hyperparameters(
     num_epochs=100,
     verbose=0,
     callbacks=dict(
-        # CSVExtendedLogger=dict(),
         ReduceLROnPlateau=dict(monitor="val_r2", mode="max", verbose=0, patience=5),
         EarlyStopping=dict(
             monitor="val_r2", min_delta=0, mode="max", verbose=0, patience=10
