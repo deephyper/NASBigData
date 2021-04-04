4390ed526780b87b61b869898f92d7830368d58c
Deathn0t <romainegele@gmail.com>
2021-04-04 13:21:33 +0200

    adding problem_ae for attn

---
+++ nas_big_data/attn/problem_ae.py
@@ -0,0 +1,36 @@
+from deephyper.problem import NaProblem
+from nas_big_data.combo.search_space_shared import create_search_space
+from nas_big_data.attn.load_data import load_data_cache
+
+Problem = NaProblem(seed=2019)
+
+Problem.load_data(load_data_cache)
+
+Problem.search_space(create_search_space, num_layers=5)
+
+Problem.hyperparameters(
+    lsr_batch_size=True,
+    lsr_learning_rate=True,
+    batch_size=32,
+    learning_rate=0.001,
+    optimizer="adam",
+    num_epochs=100,
+    verbose=0,
+    callbacks=dict(
+        ReduceLROnPlateau=dict(monitor="val_r2", mode="max", verbose=0, patience=5),
+        EarlyStopping=dict(
+            monitor="val_r2", min_delta=0, mode="max", verbose=0, patience=10
+        ),
+        )
+)
+
+Problem.loss("categorical_crossentropy")
+
+Problem.metrics(["f1_score"])
+
+Problem.objective("val_f1_score")
+
+
+# Just to print your problem, to test its definition and imports in the current python environment.
+if __name__ == "__main__":
+    print(Problem)
