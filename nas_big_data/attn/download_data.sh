2492f8a7c0ecb59b303584154f080924d683335c
Deathn0t <romainegele@gmail.com>
2021-04-04 11:02:50 +0200

    debug

--- nas_big_data/attn/download_data.sh
+++ nas_big_data/attn/download_data.sh
@@ -2,4 +2,4 @@
 
 git clone --depth=1 --branch=master https://github.com/deephyper/Benchmarks.git
 rm -rf Benchmarks/.git/
-python Benchmarks/Pilot1/Combo/combo_loader.py
+python Benchmarks/Pilot1/Attn/attn_loader.py
