60404fa81fdc1a1293aa2025e8f93fb22c45c5e7
Deathn0t <romainegele@gmail.com>
2021-04-06 10:28:00 +0200

    update

---
+++ nas_big_data/attn/class_weights.py
@@ -0,0 +1,11 @@
+import numpy as np
+from nas_big_data.attn.load_data import load_data_h5
+from sklearn.utils.class_weight import compute_class_weight
+
+_, y_train = load_data_h5("train")
+
+y_integers = np.argmax(y_train, axis=1)
+class_weights = compute_class_weight("balanced", np.unique(y_integers), y_integers)
+d_class_weights = dict(enumerate(class_weights))
+print(class_weights)
+print(d_class_weights)
